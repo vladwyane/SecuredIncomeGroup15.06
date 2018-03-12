af15d0270e2677f987efa6cf6c00c422ed712c35
DESKTOP-68UBOBQ\Vlad <vladwyane@gmail.com>
2018-03-13 00:00:19 +0200

    Step2Account

--- src/main/java/create/account/Step2Account.java
+++ src/main/java/create/account/Step2Account.java
@@ -1,7 +1,12 @@
 package create.account;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.yandex.qatools.htmlelements.annotations.Block;
+import ru.yandex.qatools.htmlelements.annotations.Name;
+import ru.yandex.qatools.htmlelements.element.*;
 import ru.yandex.qatools.htmlelements.loader.HtmlElementLoader;
 
 public class Step2Account {
@@ -14,4 +19,111 @@ public class Step2Account {
         wait = new WebDriverWait(driver, 3);
         this.driver = driver;
     }
+
+    @Name("First Name")
+    @FindBy(id = "first-name")
+    private TextInput firstName;
+
+    public void enterFirstName(String fName) {
+        firstName.sendKeys(fName);
+    }
+
+    public String getValueFirstName() {
+        return firstName.getEnteredText();
+    }
+
+    @Name("Last Name")
+    @FindBy(id = "last-name")
+    private TextInput lastName;
+
+    public void enterLastName(String lName) {
+        lastName.sendKeys(lName);
+    }
+
+    public String getValueLastName() {
+        return lastName.getEnteredText();
+    }
+
+    @Name("Birth Date")
+    @FindBy(id = "date")
+    private TextInput birthDate;
+
+    public void enterBirthDate(String date) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.getElementById('date').value='" + date + "'");
+    }
+
+    public String getValueBirthDate() {
+        return birthDate.getEnteredText();
+    }
+
+    @Name("Social Security Number")
+    @FindBy(id = "ssn")
+    private TextInput sSN;
+
+    public void enterSSN(String number) {
+        sSN.sendKeys(number);
+    }
+
+    public String getValueSSN() {
+        return sSN.getEnteredText();
+    }
+
+    @Name("Citizenship US Citizen")
+    @FindBy(xpath = "//span[text()='US Citizen']")
+    private HtmlElement citizenUS;
+
+    public void checkedCitizenUS() {
+        citizenUS.click();
+    }
+
+    @Name("Foreign National")
+    @FindBy(xpath = "//span[text()='Foreign National']")
+    private HtmlElement citizenFN;
+
+    public void checkedCitizenFN() {
+        citizenFN.click();
+    }
+
+    @Name("Street Address")
+    @FindBy(id = "street-address")
+    private TextInput streetAdress;
+
+    public void enterStreetAddress(String adress) {
+        streetAdress.sendKeys(adress);
+    }
+
+    public String getStreetAddress() {
+        return streetAdress.getEnteredText();
+    }
+
+    @Name("Country Select")
+    @FindBy(xpath = "//div[@id='investor-section']//span[contains(@class, 'country-select')]")
+    private HtmlElement countrySelect;
+
+    @Name("Country Canada")
+    @FindBy(xpath = "//span[contains(text(), 'Canada')]")
+    private HtmlElement countryCanada;
+
+    public void chooseCountryCanada() {
+        countrySelect.click();
+        countryCanada.click();
+    }
+
+    @Name("United States")
+    @FindBy(xpath = "//span[contains(text(), 'United States')]")
+    private HtmlElement countryUS;
+
+    public void chooseCountryUS() {
+        countrySelect.click();
+        countryUS.click();
+    }
+
+
+
+
+
+
+
 }
+
--- src/test/java/create/account/test/Step2AccountTest.java
+++ src/test/java/create/account/test/Step2AccountTest.java
@@ -1,4 +1,40 @@
 package create.account.test;
 
-public class Step2AccountTest {
+import create.account.Step1Account;
+import create.account.Step2Account;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import registration.pages.Register;
+import test.base.TestBase;
+import user.pages.UserAccounts;
+
+public class Step2AccountTest extends TestBase{
+
+    @BeforeMethod
+    public void createAccount() {
+        app.goTo("http://securedincomegroup.stgng.co/");
+        Register register = new Register(app.getDriver());
+        register.signIn(app.testEmail, app.correctPassword);
+        UserAccounts userAccounts = new UserAccounts(app.getDriver());
+        userAccounts.clickOpenAccountLink();
+        Step1Account step1Account = new Step1Account(app.getDriver());
+        step1Account.chooseAccountIndividual();
+        step1Account.clickSaveButton();
+    }
+
+
+    @Test
+    public void testCancelButton() throws InterruptedException {
+
+        Step2Account step2Account = new Step2Account(app.getDriver());
+        step2Account.enterBirthDate("29/11/2015");
+        step2Account.enterSSN("123");
+        step2Account.checkedCitizenFN();
+        step2Account.enterStreetAddress("Alch");
+        step2Account.chooseCountryUS();
+        Thread.sleep(3000);
+       // app.sAssert().assertEquals(step2Account.getValueFirstName(), "t");
+       // app.sAssert().assertAll();
+    }
+
 }
